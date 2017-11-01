5b5d3f072f8102fce1d6dbacac1f6962defba7ad
Drew Bragg <DRBragg@gmail.com>
2017-11-01 15:52:50 -0400

    added mock data

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,32 @@ export default {
   components: {
     TodoList,
   },
+  data() {
+    return {
+      todos: [
+        {
+          title: 'Todo A',
+          project: 'Project A',
+          done: false,
+        },
+        {
+          title: 'Todo B',
+          project: 'Project B',
+          done: true,
+        },
+        {
+          title: 'Todo C',
+          project: 'Project C',
+          done: false,
+        },
+        {
+          title: 'Todo D',
+          project: 'Project D',
+          done: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
